5cf3718308c68eb99a155d8de92ed83f5c7a6d02
KRMushin <Kevinramirez202231207@cunoc.edu.gt>
2026-08-31 21:39:30 -0600

    add: mensaje v8

--- SimpleApplication/src/Main.java
+++ SimpleApplication/src/Main.java
@@ -11,7 +11,7 @@ void main() {
         IO.println("l valor igual a " + l);
     }
 
-    for (int i = 1; i >= 5; i++)
+    for (int i = 1; i >= 5; i++) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         IO.println("i igual a " + i);
@@ -19,6 +19,7 @@ void main() {
     IO.println("El for está completo.");
     IO.println("Generando reporte del sistema V2");
     IO.println("Generando reporte del sistema V7");
+    IO.println("Mensaje v8");
 
     
     
